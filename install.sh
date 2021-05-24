5bee21e5169daf4fafd3e92755b7a3612d73f7c7
rogerpanza <matteo111209@gmail.com>
2021-05-24 13:42:48 -0400

    relhgviugfb

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Creating config directories..."
-mkdir ~/.config/picom ~/.config/bspwm ~/.config/sxhkd ~/.config/kitty ~/.config/polybar ~/.xmonad ~/.config/nvim ~/Downloads
+mkdir ~/.config/picom ~/.config/bspwm ~/.config/sxhkd ~/.config/kitty ~/.config/polybar ~/.xmonad ~/.config/nvim ~/Downloads ~/.config/rofi
 echo
 echo "Copying files..."
 cp -r ./kitty/* ~/.config/kitty/
@@ -10,6 +10,7 @@ cp -r ./bspwm/* ~/.config/bspwm/
 cp -r ./sxhkd/* ~/.config/sxhkd/
 cp -r ./polybar/* ~/.config/polybar/
 cp -r ./xmonad/* ~/.xmonad/
+cp -r ./rofi/* ~/.config/rofi/
 echo "Copying wallpapers to ~/Downloads"
 mkdir ~/wallpapers
 cp wallpapers/* ~/wallpapers/
